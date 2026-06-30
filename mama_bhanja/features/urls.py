fb6379cf1bc6105836e1da19c81771ca64a9134c
ridtech2405 <ridtech2405@gmail.com>
2026-07-01 00:27:09 +0530

    update files

--- mama_bhanja/features/urls.py
+++ mama_bhanja/features/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('models/', views.models, name='models'),
-    path('projects/', views.projects, name='projects'),
-    path('gallery/', views.gallery, name='gallery'),
-    path('pricing/', views.pricing, name='pricing'),
-    path('career/', views.career, name='career'),
-    path('contact/', views.contact, name='contact'),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("models/", views.models, name="models"),
+    path("projects/", views.projects, name="projects"),
+    path("gallery/", views.gallery, name="gallery"),
+    path("pricing/", views.pricing, name="pricing"),
+    path("career/", views.career, name="career"),
+    path("contact/", views.contact, name="contact"),
 ]
--- mama_bhanja/mama_bhanja/urls.py
+++ mama_bhanja/mama_bhanja/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('features.urls')),
-]
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("features.urls")),
+]
 
 
 
